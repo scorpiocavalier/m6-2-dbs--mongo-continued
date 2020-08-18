500d9347f3945f942337914357d8e712746f68a8
Arturo Chen <art.can.fly@gmail.com>
2020-08-18 14:46:07 -0400

    added dotenv

--- server/routes.js
+++ server/routes.js
@@ -1,50 +1,56 @@
-const router = require("express").Router();
+const router = require("express").Router()
 
-const NUM_OF_ROWS = 8;
-const SEATS_PER_ROW = 12;
+///////////////////////////////////////////////////////////////
 
-// Code that is generating the seats.
-// ----------------------------------
-const seats = {};
-const row = ["A", "B", "C", "D", "E", "F", "G", "H"];
+const seats = {}
+const NUM_OF_ROWS = 8
+const SEATS_PER_ROW = 12
+let state, lastBookingAttemptSucceeded = false
+const row = [ "A", "B", "C", "D", "E", "F", "G", "H" ]
+
+///////////////////////////////////////////////////////////////
+
 for (let r = 0; r < row.length; r++) {
   for (let s = 1; s < 13; s++) {
-    seats[`${row[r]}-${s}`] = {
-      price: 225,
+    seats[ `${ row[ r ] }-${ s }` ] = {
+      price: 400 - r * 25,
       isBooked: false,
-    };
+    }
   }
 }
-// ----------------------------------
-//////// HELPERS
+
+///////////////////////////////////////////////////////////////
+
 const getRowName = (rowIndex) => {
-  return String.fromCharCode(65 + rowIndex);
-};
+  return String.fromCharCode(65 + rowIndex)
+}
+
+///////////////////////////////////////////////////////////////
 
 const randomlyBookSeats = (num) => {
-  const bookedSeats = {};
+  const bookedSeats = {}
 
   while (num > 0) {
-    const row = Math.floor(Math.random() * NUM_OF_ROWS);
-    const seat = Math.floor(Math.random() * SEATS_PER_ROW);
+    const row = Math.floor(Math.random() * NUM_OF_ROWS)
+    const seat = Math.floor(Math.random() * SEATS_PER_ROW)
 
-    const seatId = `${getRowName(row)}-${seat + 1}`;
+    const seatId = `${ getRowName(row) }-${ seat + 1 }`
 
-    bookedSeats[seatId] = true;
+    bookedSeats[ seatId ] = true
 
-    num--;
+    num--
   }
 
-  return bookedSeats;
-};
+  return bookedSeats
+}
 
-let state;
+///////////////////////////////////////////////////////////////
 
 router.get("/api/seat-availability", async (req, res) => {
   if (!state) {
     state = {
       bookedSeats: randomlyBookSeats(30),
-    };
+    }
   }
 
   return res.json({
@@ -52,50 +58,52 @@ router.get("/api/seat-availability", async (req, res) => {
     bookedSeats: state.bookedSeats,
     numOfRows: 8,
     seatsPerRow: 12,
-  });
-});
+  })
+})
 
-let lastBookingAttemptSucceeded = false;
+///////////////////////////////////////////////////////////////
 
 router.post("/api/book-seat", async (req, res) => {
-  const { seatId, creditCard, expiration } = req.body;
+  const { seatId, creditCard, expiration } = req.body
 
   if (!state) {
     state = {
       bookedSeats: randomlyBookSeats(30),
-    };
+    }
   }
 
-  const isAlreadyBooked = !!state.bookedSeats[seatId];
+  const isAlreadyBooked = !!state.bookedSeats[ seatId ]
   if (isAlreadyBooked) {
     return res.status(400).json({
       message: "This seat has already been booked!",
-    });
+    })
   }
 
   if (!creditCard || !expiration) {
     return res.status(400).json({
       status: 400,
       message: "Please provide credit card information!",
-    });
+    })
   }
 
   if (lastBookingAttemptSucceeded) {
-    lastBookingAttemptSucceeded = !lastBookingAttemptSucceeded;
+    lastBookingAttemptSucceeded = !lastBookingAttemptSucceeded
 
     return res.status(500).json({
       message: "An unknown error has occurred. Please try your request again.",
-    });
+    })
   }
 
-  lastBookingAttemptSucceeded = !lastBookingAttemptSucceeded;
+  lastBookingAttemptSucceeded = !lastBookingAttemptSucceeded
 
-  state.bookedSeats[seatId] = true;
+  state.bookedSeats[ seatId ] = true
 
   return res.status(200).json({
     status: 200,
     success: true,
-  });
-});
+  })
+})
 
-module.exports = router;
+///////////////////////////////////////////////////////////////
+
+module.exports = router
--- server/server.js
+++ server/server.js
@@ -1,14 +1,12 @@
-const express = require('express');
-const morgan = require('morgan');
+require('dotenv').config()
+const {PORT} = process.env
+const express = require('express')
+const morgan = require('morgan')
 
-const PORT = 5678;
+const app = express()
 
-var app = express();
+app.use(express.json())
+app.use(morgan('dev'))
+app.use(require('./routes'))
 
-app.use(express.json());
-app.use(morgan('dev'));
-app.use(require('./routes'));
-
-const server = app.listen(PORT, function () {
-  console.info('🌍 Listening on port ' + server.address().port);
-});
+app.listen(PORT, () => console.info(`🌍 Listening on port ${PORT}`))
